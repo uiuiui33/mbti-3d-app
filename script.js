1802f61cb22a34b88e84447ae94770d8edd04e25
uiuiui33 <aswwes115@gmail.com>
2025-02-27 02:34:35 +0900

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.min.js';
+
+console.log("Three.js is successfully imported!");
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
