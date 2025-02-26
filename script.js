ee78ed700d5f06b9066e4c368cd2edba87f16903
uiuiui33 <aswwes115@gmail.com>
2025-02-27 04:46:15 +0900

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.min.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/controls/OrbitControls.js';
 
 console.log("Three.js is successfully imported!");
